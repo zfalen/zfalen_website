0577b5765216b83db4f04756f07b2ebb7bc63bcd
zfalen <zach.falen@partnerscreative.com>
2015-11-25 18:23:24 -0700

    update for production

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '912958028797010', // your App ID
         'clientSecret'  : '46d438c2ad7802284553d9b21fe98e6a', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL'   : 'http://www.zachfalen.com/auth/facebook/callback'
     }
 
 };
